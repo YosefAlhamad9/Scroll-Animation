00dec199428144b70d110b88baed36d50464fb63
Yosef Alhamad <62147730+YosefAlhamad9@users.noreply.github.com>
2022-04-30 12:15:57 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,39 @@
+﻿const intro = document.querySelector(".intro");
+const video = intro.querySelector("video");
+const text = intro.querySelector("h1");
+
+const section = document.querySelector("section");
+const end = section.querySelector("h1");
+
+const controller = new ScrollMagic.Controller();
+const textAnim = TweenMax.fromTo(text, 3, { opacity: 1 }, { opacity: 0 });
+
+let scene2 = new ScrollMagic.Scene({
+  duration: 3000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .setTween(textAnim)
+  .addTo(controller);
+
+let scene = new ScrollMagic.Scene({
+  duration: 11000,
+  triggerElement: intro,
+  triggerHook: 0,
+})
+  .addIndicators()
+  .setPin(intro)
+  .addTo(controller);
+
+let accelamount = 0.1;
+let scrollpos = 0;
+let delay = 0;
+
+scene.on("update", (e) => {
+  scrollpos = e.scrollPos / 1000;
+});
+
+setInterval(() => {
+  delay = (scrollpos - delay) * accelamount;
+  video.currentTime = scrollpos;
+}, 76.9);
